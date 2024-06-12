96960b854463d7784d011a7d8bb081e6df0301fb
Neko24072 <6629011006@cdti.ac.th>
2024-06-12 16:00:47 +0700

    update

---
+++ lab2.3.py
@@ -0,0 +1,23 @@
+print("หาค่าBMI")
+
+x = float(input("ใส่น้ำหนักแบบกิโลกรัม: "))
+
+y = float(input("ใส่สูงแบบเซนติเมตร: "))
+
+z = x/ (y/100*y/100)
+
+print("ค่าBMI %.2f" %z)
+
+if z < 18.5:
+    print("ผอม")
+    
+elif z >= 18.5 and z < 23:
+    print("ปกติ")
+    
+elif z >= 23 and z < 25:
+    print("ท้วม")
+    
+elif z >= 25 and z < 30:
+    print("อ้วน")
+    
+else: print("อ้วนมาก")
